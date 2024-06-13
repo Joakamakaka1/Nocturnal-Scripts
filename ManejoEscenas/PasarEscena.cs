494d5f6ba26165778ef065fc2cd84e30db9d7bae
Joakamakaka1 <joaquinfutbolero11@gmail.com>
2024-06-13 20:06:29 +0200

    Scripts ordenados

--- ManejoEscenas/PasarEscena.cs
+++ ManejoEscenas/PasarEscena.cs
@@ -16,11 +16,6 @@ public class PasarEscena : MonoBehaviour
         Application.Quit();
     }
 
-    public void Opciones()
-    {
-        SceneManager.LoadScene("Opciones");
-    }
-
     public void Credito()
     {
         Application.OpenURL("https://edermunizz.itch.io/free-pixel-art-forest/download/eyJpZCI6MTIxNjU4LCJleHBpcmVzIjoxNzE2MDQ1Mjg4fQ%3d%3d.jVD9zsUE44LHNWvtMYfoehYcy18%3d");
