bcfb7fd5892582a3ef5a8f8b16ffbf63f4ea4afa
Joakamakaka1 <joaquinfutbolero11@gmail.com>
2024-06-19 22:10:53 +0200

    Modificaciones subidas

--- Otros/SocialMediaLinks.cs
+++ Otros/SocialMediaLinks.cs
@@ -6,26 +6,26 @@ public class SocialMediaLinks : MonoBehaviour
 {
     public void Git()
     {
-        Application.OpenURL("https://edermunizz.itch.io/free-pixel-art-forest/download/eyJpZCI6MTIxNjU4LCJleHBpcmVzIjoxNzE2MDQ1Mjg4fQ%3d%3d.jVD9zsUE44LHNWvtMYfoehYcy18%3d");
+        Application.OpenURL("https://github.com/Joakamakaka1");
     }
 
     public void Discord()
     {
-        Application.OpenURL("https://edermunizz.itch.io/free-pixel-art-forest/download/eyJpZCI6MTIxNjU4LCJleHBpcmVzIjoxNzE2MDQ1Mjg4fQ%3d%3d.jVD9zsUE44LHNWvtMYfoehYcy18%3d");
+        Application.OpenURL("https://github.com/Joakamakaka1");
     }
 
     public void Itchio()
     {
-        Application.OpenURL("https://edermunizz.itch.io/free-pixel-art-forest/download/eyJpZCI6MTIxNjU4LCJleHBpcmVzIjoxNzE2MDQ1Mjg4fQ%3d%3d.jVD9zsUE44LHNWvtMYfoehYcy18%3d");
+        Application.OpenURL("https://github.com/Joakamakaka1");
     }
 
     public void Gmail()
     {
-        Application.OpenURL("https://edermunizz.itch.io/free-pixel-art-forest/download/eyJpZCI6MTIxNjU4LCJleHBpcmVzIjoxNzE2MDQ1Mjg4fQ%3d%3d.jVD9zsUE44LHNWvtMYfoehYcy18%3d");
+        Application.OpenURL("https://github.com/Joakamakaka1");
     }
 
     public void Youtube()
     {
-        Application.OpenURL("https://edermunizz.itch.io/free-pixel-art-forest/download/eyJpZCI6MTIxNjU4LCJleHBpcmVzIjoxNzE2MDQ1Mjg4fQ%3d%3d.jVD9zsUE44LHNWvtMYfoehYcy18%3d");
+        Application.OpenURL("https://github.com/Joakamakaka1");
     }
 }
